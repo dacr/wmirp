e200dd55fd92a9d4c910e4201d2891693e37a91b
Crosson David <crosson.david@gmail.com>
2014-04-18 10:05:19 +0200

    http server skeleton part is working fine

--- build.sbt
+++ build.sbt
@@ -1,6 +1,16 @@
+import AssemblyKeys._
+
+seq(assemblySettings: _*)
+
+name := "wmirp"
+
+version := "0.1"
 
 scalaVersion := "2.10.4"
 
-libraryDependencies ++= Dependencies.akka
+mainClass in assembly := Some("fr.janalyse.wmirp.Main")
 
+jarName in assembly := "wmirp.jar"
+
+libraryDependencies ++= Dependencies.akka
 
---
+++ src/main/scala/fr/janalyse/wmirp/Main.scala
@@ -0,0 +1,32 @@
+package fr.janalyse.wmirp
+
+import akka.io.IO
+import spray.can.Http
+import akka.actor._
+import spray.http._
+import HttpMethods._
+
+object ListenerActor {
+  def props() = Props(new ListenerActor)
+}
+
+class ListenerActor extends Actor {
+  def receive = {
+    case _: Http.Connected => sender ! Http.Register(self)
+    case HttpRequest(GET, Uri.Path("/ping"), _, _, _) =>
+      sender() ! HttpResponse(entity = "PONG")
+    case HttpRequest(_,_,_,_,_) => 
+      sender() ! HttpResponse(status=404, entity="NotFound")
+  }
+}
+
+object Main {
+
+  def main(args: Array[String]): Unit = {
+    implicit val system = ActorSystem()
+
+    val myListener: ActorRef = system.actorOf(ListenerActor.props)
+
+    IO(Http) ! Http.Bind(myListener, interface = "localhost", port = 9900)
+  }
+}
